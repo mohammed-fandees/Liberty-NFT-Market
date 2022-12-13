2efafcb064965aeb7e9b32879c8248fd5da36cb9
Mohammed Fandees <mohammed.fandees@gmail.com>
2022-12-13 15:55:15 +0200

    add project files

---
+++ js/script.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    document.getElementById("click").style.display = "flex";
+}
+
+window.alert('This does not reflect the latest version of the site, it is still under development, so you may find some illogical things, Enjoy...')
